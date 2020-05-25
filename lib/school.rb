2c953c4de728eba83072581a9c7dc2f00addd723
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-25 01:28:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
-# code here!
+class School
+        def initialize(school_name)
+            @school_name = school_name
+            @roster = {}
+        end
+
+        def roster 
+          @roster
+        end
+
+        def add_student (name, grade)
+            roster[grade] ||= []
+            roster[grade] << name
+            roster
+        end
+
+        def grade (grade)
+          roster[grade]
+        end
+
+        def sort
+          new_sorted_hash = {}
+            roster.each do |grade, name|
+                new_sorted_hash[grade] = name.sort
+              end
+          new_sorted_hash 
+        end
