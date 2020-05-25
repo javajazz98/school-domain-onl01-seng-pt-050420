f7aa1eb8fae055fa0d70de366502a9da1dcd8e2a
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-25 01:31:54 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,27 +1,38 @@
+# code here!
 class School
-        def initialize(school_name)
-            @school_name = school_name
-            @roster = {}
-        end
 
-        def roster 
-          @roster
-        end
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
 
-        def add_student (name, grade)
-            roster[grade] ||= []
-            roster[grade] << name
-            roster
-        end
+    def name
+        @name
+    end
 
-        def grade (grade)
-          roster[grade]
-        end
+    def roster
+        @roster
+    end
 
-        def sort
-          new_sorted_hash = {}
-            roster.each do |grade, name|
-                new_sorted_hash[grade] = name.sort
-              end
-          new_sorted_hash 
+    def add_student(name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = []
+            @roster[grade] << name
         end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+        sorted
+    end
+
+end
